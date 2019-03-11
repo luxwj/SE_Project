d7f153eb2893687c670c67c9928c5d696dba79af
luxwj <67500027@qq.com>
2019-03-11 21:28:24 +0800

    Switch Main Menu

--- Assets/Scripts/GameToMenu.cs
+++ Assets/Scripts/GameToMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameToMenu : MonoBehaviour {
 
@@ -11,6 +12,8 @@ public class GameToMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			SceneManager.LoadScene("Menu");
+		}
 	}
 }
