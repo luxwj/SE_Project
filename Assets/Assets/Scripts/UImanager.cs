2867020a2d28ab4062d94ffdd7c32215921002a3
RiccardoYeung <48344899+RiccardoYeung@users.noreply.github.com>
2019-03-22 12:59:35 +0800

    Scenetest

---
+++ Assets/Assets/Scripts/UImanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UImanager : MonoBehaviour
+{
+	public Text ScoreBoard;
+	public int score = 0;
+	// Start is called before the first frame update
+	public void ClearScore()
+	{
+		score = 0;
+
+
+
+	}
+	public GameObject newImage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		score += 1;
+		ScoreBoard.text = score.ToString();
+		if (score > 100)
+		{
+			newImage.SetActive(true);
+		}
+
+    }
+}
