b8ebb41aa0ee1ae11ec5bd272b566847a92b8bcb
luxwj <67500027@qq.com>
2019-03-25 20:34:43 +0800

    Add WangJue Folders

---
+++ Assets/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Add something
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Text scoreBoard;
+
+	public int number = 0;
+
+	public GameObject newImage;
+
+	void Update()
+	{
+		number+=1;
+		scoreBoard.text = number.ToString();
+		if (number > 100) {
+			newImage.SetActive(true);
+		}
+	}
+    public void ClearScore()
+	{
+		number = 0;
+ 
+	}
+}
