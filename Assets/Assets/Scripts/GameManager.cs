c262dcaf861b97ce2c97a17843f8b01b78daba52
WNDCCJ <48348513+WNDCCJ@users.noreply.github.com>
2019-04-27 15:06:00 +0800

    1
    11

---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    public Transform startplayer1;
+    public Transform startplayer2;
+    public GameObject Ballprefeb;
+    public static bool serve = false;
+
+    //private bool isover = false;
+    private int MatchNO=1;
+
+    //private RubberBall1 currentball;
+
+    // Use this for initialization
+    void Start () {
+        GameObject.Instantiate(Ballprefeb, startplayer1.position, Ballprefeb.transform.rotation);
+    }
+	
+    void BallServe(int n)
+    {
+        serve = (n % 2 == 0) ? true : false;
+        if (serve)
+        {
+            GameObject.Instantiate(Ballprefeb, startplayer2.position, Ballprefeb.transform.rotation);
+        }
+        else
+        {
+            GameObject.Instantiate(Ballprefeb, startplayer1.position, Ballprefeb.transform.rotation);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+       
+	}
+    public void Match()
+    {
+        //if (isover) return;
+        //isover = true;
+        MatchNO += 1;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        BallServe(MatchNO);
+    }
+}
---
+++ Assets/Assets/Scripts/ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    public float speed = 100;
+    private Rigidbody Ball;
+
+    // Use this for initialization
+    void Start () {
+        Ball = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bat1")
+        {
+            GameManager.serve = true;
+            Vector3 nowVel = Ball.velocity;
+            Ball.velocity = new Vector3(speed, speed, nowVel.z);
+        }
+        if (collision.gameObject.name == "Bat2")
+        {
+            GameManager.serve = false;
+            Vector3 nowVel = Ball.velocity;
+            Ball.velocity = new Vector3(-speed, speed, nowVel.z);
+        }
+        if (collision.collider.tag == "wall")
+        {
+            Destroy(gameObject);
+            GameObject.Find("Gamemanager").GetComponent<GameManager>().Match();
+            //Destroy(gameObject);
+        }
+    }
+}
