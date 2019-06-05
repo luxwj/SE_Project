619502f64973b9a61873c45ffb2e73e4401c758a
RiccardoYeung <48344899+RiccardoYeung@users.noreply.github.com>
2019-06-05 15:58:28 +0800

    Upload TrainingMode
    Upload training mode

---
+++ Assets/Assets/Scripts/OnePlayerTraining/Ball1PT.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball1PT : Ball {
+    
+    public float _speed;
+    protected override void InitBall() {
+        m_Rb = GetComponent<Rigidbody>();
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController1PT>();
+        m_Rb.AddForce((Vector3.up - Vector3.right) * _speed, ForceMode.VelocityChange);
+    }
+
+    protected override void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Player1") {
+            GetBatHit(other);
+        } else if (other.gameObject.tag == "Boundary") {
+            Destroy(gameObject);
+            GameController1PT._instance.StartGame();
+        }
+    }
+
+    protected override void OnCollisionStay(Collision other) {
+        
+    }
+
+    /// <summary>
+    /// Check for trigger over
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Star")
+        {
+            
+            GameController1PT._instance.Score++;
+            Destroy(other.gameObject);
+        }
+         
+    }
+}
---
+++ Assets/Assets/Scripts/OnePlayerTraining/PlayerBat1PT.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBat1PT : PlayerBat {
+
+    protected override void InitPlayer() {
+        base.InitPlayer();
+    }
+
+    protected override void Move() {
+        base.Move();
+    }
+
+    protected override void Bat() {
+        base.Bat();
+    }
+
+    protected override void CheckMoveBoundary() {
+        base.CheckMoveBoundary();
+    }
+
+    public override void ResetBatState() {
+        base.ResetBatState();
+    }
+}
