cb14d677972bdc0401a0a1f27d9dbf4b6e81c8ab
XianYuCZ <48359477+XianYuCZ@users.noreply.github.com>
2019-06-11 21:40:20 +0800

    修改mainmenu

--- Assets/Assets/Scripts/MainMenu/Login_user_xycz.cs
+++ Assets/Assets/Scripts/MainMenu/Login_user_xycz.cs
@@ -3,22 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Login_user_xycz : MonoBehaviour {
 
     //名字提示框
     public Text labeluser;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         string userName="";
         userName = PlayerPrefs.GetString("userlogin");
         labeluser.text = "用户名：" + userName;
         PlayerPrefs.DeleteKey("userlogin");
-	}
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
